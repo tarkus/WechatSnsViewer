d30ea450a2af5443b955265e6053fea8f11660e4
Tarkus Liu <tarkus.liu@gmail.com>
2020-03-04 12:59:47 +0800

    better generating workflow

--- word.py
+++ word.py
@@ -3,10 +3,14 @@ import json
 import jieba.analyse
 import imageio
 import sys
+import os
 from wordcloud import WordCloud
 
 import matplotlib
 matplotlib.use('TkAgg')
+
+jsonFile = "./data/exported_sns.json";
+outputFile = "./data/{AUTHOR_ID}/words.png"
 
 
 def gen_img(texts, img_file):
@@ -15,20 +19,28 @@ def gen_img(texts, img_file):
     wc = WordCloud(
         background_color='white',
         mask=image_coloring,
-        font_path='/Library/Fonts/Songti.ttc' # 替换成你需要的字体目录
+        font_path='/Library/Fonts/Songti.ttc'
     )
     wc.generate(data)
-    wc.to_file('./outputs/word.png')
+    output = outputFile.replace('{AUTHOR_ID}', authorId)
+    outputDir = os.path.dirname(output)
+    os.makedirs(outputDir, exist_ok=True)
+    wc.to_file(output)
     print("成功生成词云")
 
 
 if __name__ == '__main__':
-    snsInfos = json.loads(open('./datas/data.json', 'r', encoding='utf-8').read())
-    print('朋友圈总数：', len(snsInfos))
+    authorId = ""
+    timeline = json.loads(open(jsonFile, 'r', encoding='utf-8').read())
+    print('朋友圈总条目数：', len(timeline))
 
     words = []
-    for snsInfo in snsInfos:
-        words.extend(jieba.analyse.extract_tags(snsInfo['content']))
+    for entry in timeline:
+        if authorId and authorId != entry['authorId']:
+            raise ValueError('inconsistent authorId')
+        else:
+            authorId = entry['authorId']
+        words.extend(jieba.analyse.extract_tags(entry['content']))
     print("总词数：", len(words))
 
     gen_img(words, 'assets/wechat.png')
